49c4d7c11beb472a45c69ed569d4bc09966f9d26
linda09 <z5113943@ad.unsw.edu.au>
2019-06-22 10:45:58 +1000

    added Property model to database

--- mysite/booking/models.py
+++ mysite/booking/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Property(models.Model):
+    title = models.TextField()
+    size = models.DecimalField(max_digits=15, decimal_places=2)
+
+
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # own apps
+    'booking',
+    'forum',
+    'review',
 ]
 
 MIDDLEWARE = [
