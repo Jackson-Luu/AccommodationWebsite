6646591879442775ed5d9b2509743204b24ddd3e
linda09 <z5113943@ad.unsw.edu.au>
2019-06-22 15:09:37 +1000

    added home view, template directory, home url

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from booking.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', home_view),
 ]
