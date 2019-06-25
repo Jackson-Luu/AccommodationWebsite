5fdca9d41680fc76ba76e7d37fe6f215932d5231
Tony Dao <z5118018@ad.unsw.edu.au>
2019-06-25 14:58:43 +1000

    Added missing flags

--- mysite/booking/models.py
+++ mysite/booking/models.py
@@ -43,13 +43,13 @@ class UserReviews(models.Model):
     reviewer = models.ForeignKey('CustomUser', to_field='user_id', on_delete=models.CASCADE, related_name='reviewer')
     reviewee = models.ForeignKey('CustomUser', to_field='user_id', on_delete=models.CASCADE, related_name='reviewee')
     rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], null=False, blank=False)
-    text = models.TextField()
+    text = models.TextField(null=True, blank=True)
 
 class PropertyReviews(models.Model):
     reviewer = models.ForeignKey('CustomUser', to_field='user_id', on_delete=models.CASCADE)
     reviewee = models.ForeignKey('Property', to_field='property_id', on_delete=models.CASCADE)
     rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], null=False,blank=False)
-    text = models.TextField()
+    text = models.TextField(null=True, blank=True)
 
 class PropertyOwnership(models.Model):
     owner = models.ForeignKey('CustomUser', to_field='user_id', on_delete=models.CASCADE)
