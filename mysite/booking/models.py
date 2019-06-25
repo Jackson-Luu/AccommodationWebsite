263dd3bea4cbe84fe3a7f004e6ae7a0e24c9faab
Tony Dao <z5118018@ad.unsw.edu.au>
2019-06-25 14:52:13 +1000

    Added the rest of the db tables

--- mysite/booking/models.py
+++ mysite/booking/models.py
@@ -18,16 +18,9 @@ class CustomUser(AbstractUser):
 #     birthday = models.DateField()
 #     description = models.TextField()
 
-# class UserReviews(models.Model):
-#     reviewer = models.ForeignKey('User', to_field='user_id', on_delete=models.CASCADE)
-#     reviewee = models.ForeignKey('User', to_field='user_id', on_delete=models.CASCADE)
-#     rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], null=False, blank=False)
-#     text = models.TextField()
-
 class Property(models.Model):
     property_id = models.AutoField(primary_key=True)
-    #host_id = models.ForeignKey('CustomUser', to_field='user_id', on_delete=models.CASCADE, null=True, blank=True, default=-1) 
-    #change back to null=false , blank=false
+    host_id = models.ForeignKey('CustomUser', to_field='user_id', on_delete=models.CASCADE, default=-1)
     name = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     location = models.TextField(null=True, blank=True)
@@ -35,8 +28,37 @@ class Property(models.Model):
     description = models.TextField(null=True, blank=True)
     bookable = models.BooleanField(null=False, blank=False, default=False)
 
+class Room(models.Model):
+    room_id = models.AutoField(primary_key=True)
+    property_id = models.ForeignKey('Property', to_field='property_id', on_delete=models.CASCADE)
+    room_number = models.IntegerField(null=True, blank=True)
 
-# class Booking(models.Model):
-#
-#
-# class room(models.Model):
+class Booking(models.Model):
+    booking_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey('CustomUser', to_field='user_id', on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+class UserReviews(models.Model):
+    reviewer = models.ForeignKey('CustomUser', to_field='user_id', on_delete=models.CASCADE, related_name='reviewer')
+    reviewee = models.ForeignKey('CustomUser', to_field='user_id', on_delete=models.CASCADE, related_name='reviewee')
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], null=False, blank=False)
+    text = models.TextField()
+
+class PropertyReviews(models.Model):
+    reviewer = models.ForeignKey('CustomUser', to_field='user_id', on_delete=models.CASCADE)
+    reviewee = models.ForeignKey('Property', to_field='property_id', on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], null=False,blank=False)
+    text = models.TextField()
+
+class PropertyOwnership(models.Model):
+    owner = models.ForeignKey('CustomUser', to_field='user_id', on_delete=models.CASCADE)
+    property = models.ForeignKey('Property', to_field='property_id', on_delete=models.CASCADE)
+
+class PropertyFavourites(models.Model):
+    user = models.ForeignKey('CustomUser', to_field='user_id', on_delete=models.CASCADE)
+    property = models.ForeignKey('Property', to_field='property_id', on_delete=models.CASCADE)
+
+class BookingTable(models.Model):
+    booking = models.ForeignKey('Booking', to_field='booking_id', on_delete=models.CASCADE)
+    room = models.ForeignKey('Room', to_field='room_id', on_delete=models.CASCADE)
