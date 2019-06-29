45565b68712b3fd6f19aa10ebc64e8c7d03911d1
Tony Dao <z5118018@ad.unsw.edu.au>
2019-06-29 22:33:05 +1000

    Changed the date widget in the register form

--- mysite/user_manager/forms.py
+++ mysite/user_manager/forms.py
@@ -2,14 +2,17 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
+class DateInput(forms.DateInput):
+	input_type = 'date'
 
 class CustomUserCreationForm(UserCreationForm):
-	birthday = forms.DateField(widget=forms.SelectDateWidget)
-	description = forms.CharField(widget=forms.Textarea)
+	# birthday = forms.DateField(widget=forms.TextInput(attrs={'class':'datepicker'}))
+	# description = forms.CharField(widget=forms.Textarea)
 
 	class Meta(UserCreationForm):
 		model = CustomUser
 		fields = ['username', 'password1', 'password2', 'first_name', 'last_name', 'birthday', 'description']
+		widgets = {'birthday': DateInput(),}
 
 
 class CustomUserChangeForm(UserChangeForm):
