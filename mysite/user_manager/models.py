b83b954839e819a576abcd30d7fdf6f23616d4d9
Tony Dao <z5118018@ad.unsw.edu.au>
2019-06-29 14:10:59 +1000

    Move review models to review app

--- mysite/user_manager/models.py
+++ mysite/user_manager/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
