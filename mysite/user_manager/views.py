dd3706b83f018fd7c10ef52ac57f278f27d2a1b6
Tony Dao <z5118018@ad.unsw.edu.au>
2019-06-29 14:33:13 +1000

    More spaces to tabs

--- mysite/user_manager/views.py
+++ mysite/user_manager/views.py
@@ -4,17 +4,18 @@ from django.contrib import messages
 from .forms import CustomUserCreationForm
 from django.urls import reverse
 
+
 # Create your views here.
 def register_view(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            #username = form.cleaned_data.get('username')
-            messages.success(request,'Account created!')
-            return redirect('home')
+	if request.method == 'POST':
+		form = CustomUserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# username = form.cleaned_data.get('username')
+			messages.success(request, 'Account created!')
+			return redirect('home')
 
-    else: 
-        #print('NOT VALID')
-        form = CustomUserCreationForm()
-    return render(request, 'register.html',{'form':form})
+	else:
+		# print('NOT VALID')
+		form = CustomUserCreationForm()
+	return render(request, 'register.html', {'form': form})
